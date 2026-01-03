093ff5c944864293cae3fb0ba7a6f2188e19682c
Felipe Genuino <design@felipegenuino.com>
2026-01-03 02:33:50 -0300

    wip

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,7 +11,9 @@ class PostsController < ApplicationController
 
   # GET /posts or /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.all.order(created_at: :desc)
+    @featured_post = @posts.first
+    @remaining_posts = @posts.drop(1)
   end
 
   # GET /posts/1 or /posts/1.json
