6143df28e933da5be4df2b164f42c6a899801d74
Felipe Genuino <design@felipegenuino.com>
2026-01-04 02:02:14 -0300

    wip

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,38 +1,27 @@
 class CommentsController < ApplicationController
-  before_action :resume_session # Garante que Current.user esteja preenchido
-  before_action :set_post
-  before_action :set_comment, only: :destroy
+  # Importante: No Rails 8, isso preenche o Current.user a partir do cookie
+  before_action :resume_session 
 
   def create
+    @post = Post.find(params[:post_id])
     @comment = @post.comments.build(comment_params)
-    @comment.user = Current.user
+    
+    # Debug: vai aparecer no seu terminal onde roda o 'bin/dev'
+    puts "DEBUG: Tentando comentar como usuário: #{Current.user&.email_address}"
 
-    if @comment.save
-      redirect_to @post, notice: "Comentário publicado com sucesso!"
+    if authenticated?
+      @comment.user = Current.user
+      if @comment.save
+        redirect_to @post, notice: "Comentário publicado com sucesso!"
+      else
+        redirect_to @post, alert: "Erro nas validações: #{@comment.errors.full_messages.to_sentence}"
+      end
     else
-      redirect_to @post, alert: "O comentário não pode estar vazio."
-    end
-  end
-
-  def destroy
-    # Segurança: Só deleta se for o dono do comentário ou admin
-    if @comment.user == Current.user || Current.user&.admin?
-      @comment.destroy
-      redirect_to @post, notice: "Comentário removido."
-    else
-      redirect_to @post, alert: "Você não tem permissão para isso."
+      redirect_to new_session_path, alert: "Você precisa estar logado para comentar."
     end
   end
 
   private
-
-  def set_post
-    @post = Post.find(params[:post_id])
-  end
-
-  def set_comment
-    @comment = @post.comments.find(params[:id])
-  end
 
   def comment_params
     params.require(:comment).permit(:body)
